1f23fd9a35983da55b4a70cfbbebbdffb5ffa41e
Taro Hanamura <m@hanamurataro.com>
2016-01-13 21:54:32 +0900

    Add markdown option

--- src/engines/nunjucks.js
+++ src/engines/nunjucks.js
@@ -5,20 +5,18 @@ var nunjucks = require('nunjucks');
 var path     = require('path');
 
 module.exports = function(options) {
-
   options || (options = {});
   options.path || (options.path = 'template');
+  options.markdown || (options.markdown = function(str) {
+    if (!str) return str;
+    return new nunjucks.runtime.SafeString(marked(str));
+  });
 
   var env = new nunjucks.Environment(
     new nunjucks.FileSystemLoader(options.path),
     {noCache: true}
   );
-  env.addFilter('markdown', function(str) {
-    if (!str) {
-      return str;
-    }
-    return new nunjucks.runtime.SafeString(marked(str));
-  });
+  env.addFilter('markdown', options.markdown);
 
   return function(tmplName, tmplData) {
     return env.render(tmplName, tmplData);
